e16c0bfb5299af677e2a985d944ebda3befd1079
henry <andersonhenry38@gmail.com>
2019-08-05 09:55:40 +0700

    upload lagi

---
+++ laravel/resources/views/index.blade.php
@@ -0,0 +1,274 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" href="assets/img/favicon.png" type="image/png">
+        <title>MeetMe Personal</title>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" href="assets/vendors/linericon/style.css">
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/vendors/owl-carousel/owl.carousel.min.css">
+        <link rel="stylesheet" href="assets/vendors/lightbox/simpleLightbox.css">
+        <link rel="stylesheet" href="assets/vendors/nice-select/css/nice-select.css">
+        <link rel="stylesheet" href="assets/vendors/animate-css/animate.css">
+        <link rel="stylesheet" href="assets/vendors/popup/magnific-popup.css">
+        <link rel="stylesheet" href="assets/vendors/flaticon/flaticon.css">
+        <!-- main css -->
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/responsive.css">
+    </head>
+    <body>
+        
+        <!--================Header Menu Area =================-->
+        <header class="header_area">
+            <div class="main_menu">
+            	<nav class="navbar navbar-expand-lg navbar-light">
+					<div class="container box_1620">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<a class="navbar-brand logo_h" href="index.html"><img src="assets/img/logox.png" alt=""></a>
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+					
+					</div>
+            	</nav>
+            </div>
+        </header>
+        <!--================Header Menu Area =================-->
+        
+        <!--================Home Banner Area =================-->
+        <section class="home_banner_area">
+           	<div class="container box_1620">
+           		<div class="banner_inner d-flex align-items-center">
+					<div class="banner_content">
+						<div class="media">
+							<div class="d-flex">
+								<img src="assets/img/henry.jpg" alt="">
+							</div>
+							<div class="media-body">
+								<div class="personal_text">
+									<h6>Hallo, saya</h6>
+									<h3>Henry</h3>
+									<h4>Programmer</h4>
+									<p>Berikut biodata saya</p>
+									<ul class="list basic_info">
+										<li><a href="#"><i class="lnr lnr-calendar-full"></i> 28 Maret, 1993</a></li>
+										<li><a href="#"><i class="lnr lnr-phone-handset"></i> 082323897266</a></li>
+										<li><a href="#"><i class="lnr lnr-envelope"></i> andersonhenry38@gmail.com</a></li>
+										<li><a href="#"><i class="lnr lnr-home"></i> Purwokerto</a></li>
+									</ul>
+									<ul class="list personal_social">
+										<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+										<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+										<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+            </div>
+        </section>
+        <!--================End Home Banner Area =================-->
+        
+        <!--================Welcome Area =================-->
+        <section class="welcome_area p_120">
+        	<div class="container">
+        		<div class="row welcome_inner">
+        			<div class="col-lg-6">
+        				<div class="welcome_text">
+        					<h4>Keahlian Saya</h4>
+        					<p>berikut adalah hal-hal yang saya cukup kuasai, seperti :</p>
+        					
+        				</div>
+        			</div>
+        			<div class="col-lg-6">
+        				<div class="tools_expert">
+        					<div class="skill_main">
+								<div class="skill_item">
+									<h4>Pemprograman PHP <span class="counter">70</span>%</h4>
+									<div class="progress_br">
+										<div class="progress">
+											<div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+										</div>
+									</div>
+								</div>
+								<div class="skill_item">
+									<h4>Framework Laravel <span class="counter">50</span>%</h4>
+									<div class="progress_br">
+										<div class="progress">
+											<div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+										</div>
+									</div>
+								</div>
+								<div class="skill_item">
+									<h4>Android Java <span class="counter">70</span>%</h4>
+									<div class="progress_br">
+										<div class="progress">
+											<div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+										</div>
+									</div>
+								</div>
+								<div class="skill_item">
+									<h4>Corel Draw <span class="counter">40</span>%</h4>
+									<div class="progress_br">
+										<div class="progress">
+											<div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
+										</div>
+									</div>
+								</div>
+								
+							</div>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Welcome Area =================-->
+        
+        <!--================My Tabs Area =================-->
+        <section class="mytabs_area p_120">
+        	<div class="container">
+        		<div class="tabs_inner">
+					<ul class="nav nav-tabs" id="myTab" role="tablist">
+						<li class="nav-item">
+							<a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Pengalaman Saya</a>
+						</li>
+						
+					</ul>
+					<div class="tab-content" id="myTabContent">
+						<div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+							<ul class="list">
+								<li>
+									<span></span>
+									<div class="media">
+										<div class="d-flex">
+											<p>Maret 2013 sebagai</p>
+										</div>
+										<div class="media-body">
+							
+											<p>Operator <br />Warnet</p>
+										</div>
+									</div>
+								</li>
+								<li>
+									<span></span>
+									<div class="media">
+										<div class="d-flex">
+											<p>Januari 2015 sebagai</p>
+										</div>
+										<div class="media-body">
+											
+											<p>Programmer <br />Indokom Group</p>
+										</div>
+									</div>
+								</li>
+								<li>
+								<span></span>
+									<div class="media">
+										<div class="d-flex">
+											<p>Juni 2019 sebagai</p>
+										</div>
+										<div class="media-body">
+											<p>Programmer Api <br />PT. Visi Mutimedia Aditama (jogja)</p>
+										</div>
+									</div>
+								</li>
+							</ul>
+						</div>
+						<div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+							<ul class="list">
+								<li>
+									<span></span>
+									<div class="media">
+										<div class="d-flex">
+											<p>March 2017 to present</p>
+										</div>
+										<div class="media-body">
+											<h4>Colorlib</h4>
+											<p>Senior Web Developer <br />Santa monica, Los angeles</p>
+										</div>
+									</div>
+								</li>
+								<li>
+									<span></span>
+									<div class="media">
+										<div class="d-flex">
+											<p>March 2017 to present</p>
+										</div>
+										<div class="media-body">
+											<h4>Colorlib</h4>
+											<p>Senior Web Developer <br />Santa monica, Los angeles</p>
+										</div>
+									</div>
+								</li>
+								<li>
+								<span></span>
+									<div class="media">
+										<div class="d-flex">
+											<p>March 2017 to present</p>
+										</div>
+										<div class="media-body">
+											<h4>Colorlib</h4>
+											<p>Senior Web Developer <br />Santa monica, Los angeles</p>
+										</div>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End My Tabs Area =================-->
+        
+       
+        <!--================Footer Area =================-->
+        <footer class="footer_area p_120">
+        	<div class="container">
+        		<div class="row footer_inner">
+        			<div class="col-lg-5 col-sm-6">
+        				<aside class="f_widget ab_widget">
+        					<div class="f_title">
+        						<h3>Portofolio 2019</h3>
+        			            <h2>Tes Interview di JVM</h2>
+        		
+        					</div>
+        				
+        			</div>
+        			
+        			
+        		</div>
+        	</div>
+        </footer>
+        <!--================End Footer Area =================-->
+        
+        
+        
+        
+        
+        
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="/assets/js/jquery-3.3.1.min.js"></script>
+        <script src="/assets/js/popper.js"></script>
+        <script src="/assets/js/bootstrap.min.js"></script>
+        <script src="/assets/js/stellar.js"></script>
+        <script src="/assets/vendors/lightbox/simpleLightbox.min.js"></script>
+        <script src="/assets/vendors/nice-select/js/jquery.nice-select.min.js"></script>
+        <script src="/assets/vendors/isotope/imagesloaded.pkgd.min.js"></script>
+        <script src="/assets/vendors/isotope/isotope.pkgd.min.js"></script>
+        <script src="/assets/vendors/owl-carousel/owl.carousel.min.js"></script>
+        <script src="/assets/vendors/popup/jquery.magnific-popup.min.js"></script>
+        <script src="/assets/js/jquery.ajaxchimp.min.js"></script>
+        <script src="/assets/vendors/counter-up/jquery.waypoints.min.js"></script>
+        <script src="/assets/vendors/counter-up/jquery.counterup.min.js"></script>
+        <script src="/assets/js/mail-script.js"></script>
+        <script src="/assets/js/theme.js"></script>
+    </body>
+</html>
